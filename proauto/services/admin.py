30791f5edea085d33dd7febfddcb111f0116ba0d
sazhod <sazhod@yandex.ru>
2023-05-20 21:54:55 +0500

    Add nested_admin to admin panel

--- proauto/services/admin.py
+++ proauto/services/admin.py
@@ -1,20 +1,20 @@
 from django.contrib import admin
 from .models import ServiceModel, WorkExampleModel, ImagesModel
-from nested_inline.admin import NestedStackedInline, NestedModelAdmin
+# from nested_inline.admin import NestedStackedInline, NestedModelAdmin
+import nested_admin
 
-
-class ImagesInline(NestedStackedInline):
+class ImagesInline(nested_admin.NestedStackedInline):
     model = ImagesModel
     extra = 0
 
 
-class WorkExampleInline(NestedStackedInline):
+class WorkExampleInline(nested_admin.NestedStackedInline):
     model = WorkExampleModel
     extra = 0
     inlines = [ImagesInline]
 
 
-class ServiceAdmin(NestedModelAdmin):
+class ServiceAdmin(nested_admin.NestedModelAdmin):
     list_display = ('title', 'min_price', 'description')
     inlines = [WorkExampleInline]
 
