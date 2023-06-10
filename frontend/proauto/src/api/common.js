9927f6f09ddb2b7dfea9a9744983c25f8a267865
sazhod <sazhod@yandex.ru>
2023-06-11 01:04:05 +0500

    Update Frontend app

--- frontend/proauto/src/api/common.js
+++ frontend/proauto/src/api/common.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const backendHost = process.env.VUE_APP_BACKEND_HOST;
 const apiUrl = `http://${backendHost}:8000/api/v1/`
+
 console.log(apiUrl)
 console.log('!!!message!!!')
-export const HTTP = axios.create({
-  baseURL: apiUrl
-})
+
+export const HTTP = "";
